ba85416eea4b980e3c38a43089b295116ea9b62b
egorshv <shvedov124@gmail.com>
2022-07-04 07:06:21 +0300

    Added basic logic

---
+++ bot.py
@@ -0,0 +1,37 @@
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config import TOKEN
+from db_dispatcher import DbDispatcher
+
+from aiogram.dispatcher import FSMContext
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputFile
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+data = DbDispatcher('data.db')
+urls = DbDispatcher('urls.db')
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer('This is my bot for learning new words')
+
+
+@dp.message_handler(commands=['help'])
+async def help(message: types.Message):
+    await message.answer(
+        '/get_words - выдаёт список слов из бд\n'
+        '/add_url - возможность добавить ссылку на документацию\n')
+
+
+@dp.message_handler(commands=['get_words'])
+async def get_words(message: types.Message):
+    words = [f'{item[1]}: {item[2]}' for item in data.read_all_data('data')]
+    await message.answer('\n'.join(words))
+
+
+@dp.message_handler(commands=['add_url'])
+async def add_url(message: types.Message):
+    pass
