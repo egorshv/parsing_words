432520735ff33059b89d0c66de5af25aa094a147
egorshv <shvedov124@gmail.com>
2022-07-06 10:21:40 +0300

    Debug

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from config import TOKEN
 from db_dispatcher import DbDispatcher
 from states import AddUrl
+from parser import get_data
+from data_writing import write_data
 
 from aiogram.dispatcher import FSMContext
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputFile
@@ -31,20 +33,30 @@ async def help(message: types.Message):
 @dp.message_handler(commands=['get_words'])
 async def get_words(message: types.Message):
     words = [f'{item[1]}: {item[2]}' for item in data.read_all_data('data')]
-    await message.answer('\n'.join(words))
+    if len(words) > 0:
+        n = 0
+        while n < len(words):
+            await message.answer('\n'.join(words[n:n + 100]))
+            n += 100
+    else:
+        await message.answer('Слов пока нет')
 
 
 @dp.message_handler(commands=['add_url'])
 async def add_url(message: types.Message):
     await AddUrl.url.set()
-    await message.answer('Введите ссылку на документацию')
+    await message.answer('Введите название и ссылку на документацию через пробел')
 
 
 @dp.message_handler(state=AddUrl.url)
-async def write_url(message: types.Message):
+async def write_url(message: types.Message, state: FSMContext):
     try:
-        urls.write_data({'name': 'test', 'url': message.text}, 'urls')
+        msg_data = message.text.split()
+        urls.write_data({'name': msg_data[0], 'url': msg_data[1]}, 'urls')
         await message.answer('Запись прошла успешно')
+        await state.finish()
+        all_words = get_data()
+        write_data(all_words)
     except Exception as e:
         await message.answer(f'Что-то пошло не так\nОшибка: {e}')
 
@@ -52,7 +64,10 @@ async def write_url(message: types.Message):
 @dp.message_handler(commands=['get_urls'])
 async def get_urls(message: types.Message):
     arr = [f'{item[1]}: {item[2]}' for item in urls.read_all_data('urls')]
-    await message.answer('\n'.join(arr))
+    if len(arr) > 0:
+        await message.answer('\n'.join(arr))
+    else:
+        await message.answer('Ссылок пока нет')
 
 
 async def shutdown(dispatcher: Dispatcher):
