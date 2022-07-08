6136674faee6f2abfa6b484e1238516aae19b2d2
egorshv <shvedov124@gmail.com>
2022-07-08 16:08:53 +0300

    Debug

--- parser.py
+++ parser.py
@@ -20,12 +20,12 @@ def get_data():
     soup = BeautifulSoup(src, 'lxml')
     p_all = soup.find_all('p')
     for p in p_all:
-        symb = '123567890!@#%^&*()_+=<>/"\'[]{};:'
+        symb = '1234567890!@#%^&*()_+=<>/"\'[]{};:\\'
         line = p.text
         line = line.replace('.', '')
         line = line.replace(',', '')
         line = ''.join(x for x in line if x in string.printable)
-        text = [word for word in line.split() if not (set(symb) & set(word))]
+        text = [word for word in [i for i in line.split() if len(i) > 1] if not (set(symb) & set(word))]
         text = [word for word in text if word not in DATA]
         DATA.extend(text)
 
